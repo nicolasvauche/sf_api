cb39acc7306b5f4fe798cf6e3a3c0f9dce27aac3
Nicolas Vauche <hello@nicolasvauche.net>
2022-12-03 22:54:39 +0000

    React

---
+++ assets/Main.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './Main.scss'
+
+function Main () {
+  return (
+    <h1>Coucou</h1>
+  )
+}
+
+export default Main
+
+if (document.getElementById('app')) {
+  const rootElement = document.getElementById('app')
+  const root = createRoot(rootElement)
+
+  root.render(
+    <StrictMode>
+      <Main />
+    </StrictMode>
+  )
+}
