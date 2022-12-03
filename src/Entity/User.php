37bac2281bd17828f6df02896e5ff1d7c3098bac
Nicolas Vauche <hello@nicolasvauche.net>
2022-12-03 22:01:46 +0000

    End User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,12 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\UserRepository;
 use App\State\UserPasswordHasherProcessor;
 use Doctrine\ORM\Mapping as ORM;
@@ -13,9 +19,17 @@ use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ApiResource(
-    paginationEnabled: false,
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
     normalizationContext: ['groups' => ['get:Users']],
     denormalizationContext: ['groups' => ['post:User']],
+    paginationEnabled: false,
     processor: UserPasswordHasherProcessor::class
 )]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
