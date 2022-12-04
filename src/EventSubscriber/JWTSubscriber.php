1804c3b0ddcdc674ef700f8d95e11d6ce926ff1a
Nicolas Vauche <hello@nicolasvauche.net>
2022-12-04 14:56:21 +0000

    CRUD Users OK and Secured

--- src/EventSubscriber/JWTSubscriber.php
+++ src/EventSubscriber/JWTSubscriber.php
@@ -3,17 +3,55 @@
 namespace App\EventSubscriber;
 
 use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
 use Lexik\Bundle\JWTAuthenticationBundle\Events;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTAuthenticatedEvent;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class JWTSubscriber implements EventSubscriberInterface
 {
+
+    const REFRESH_TIME = 1800;
+    private $payload;
+    private $user;
+    private JWTTokenManagerInterface $jwtManager;
+
+    public function __construct(JWTTokenManagerInterface $jwtManager)
+    {
+        $this->jwtManager = $jwtManager;
+    }
+
     public static function getSubscribedEvents()
     {
         return [
             Events::AUTHENTICATION_SUCCESS => 'onAuthenticationSuccess',
+            Events::JWT_AUTHENTICATED => 'onAuthenticatedAccess',
+            KernelEvents::RESPONSE => 'onAuthenticatedResponse',
         ];
+    }
+
+    public function onAuthenticatedResponse(ResponseEvent $event)
+    {
+        if ($this->payload && $this->user) {
+            $expireTime = $this->payload['exp'] - time();
+            if ($expireTime < static::REFRESH_TIME) {
+                // Refresh token
+                $jwt = $this->jwtManager->create($this->user);
+                $response = $event->getResponse();
+                // Set cookie
+                $this->createCookie($response, $jwt);
+            }
+        }
+    }
+
+    public function onAuthenticatedAccess(JWTAuthenticatedEvent $event)
+    {
+        $this->payload = $event->getPayload();
+        $this->user = $event->getToken()->getUser();
     }
 
     public function onAuthenticationSuccess(AuthenticationSuccessEvent $event)
@@ -23,19 +61,24 @@ class JWTSubscriber implements EventSubscriberInterface
             $response = $event->getResponse();
             $jwt = $eventData['token'];
             // Set cookie
-            $response->headers->setCookie(
-                new Cookie(
-                    "BEARER",
-                    $jwt,
-                    new \DateTime("+1 day"),
-                    "/",
-                    null,
-                    true,
-                    true,
-                    false,
-                    'strict'
-                )
-            );
+            $this->createCookie($response, $jwt);
         }
+    }
+
+    protected function createCookie(Response $response, $jwt)
+    {
+        $response->headers->setCookie(
+            new Cookie(
+                "BEARER",
+                $jwt,
+                new \DateTime("+1 day"),
+                "/",
+                null,
+                false,
+                true,
+                false,
+                'strict'
+            )
+        );
     }
 }
